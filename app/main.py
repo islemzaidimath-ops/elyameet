303440b2a60ed1287da9340475a6832ca5c4ae22
islem <islemzadimath@gmail.com>
2026-08-03 16:01:05 +0100

    chore: clear code from main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.routers import room_router, equipment_router
+from app.routers import room_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(room_router.router)
-app.include_router(equipment_router.router)
+
